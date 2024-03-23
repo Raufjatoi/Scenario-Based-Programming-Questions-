9da6f1937c0a52e8919e8b90fbc28732bcfecc12
Abdul Rauf <141711142+Raufjatoi@users.noreply.github.com>
2024-03-23 13:12:31 +0000

    3.py

--- 3.py
+++ 3.py
@@ -6,7 +6,6 @@
 #o Track income and expenses over a chosen period (e.g., monthly, weekly).
 #o Analyze spending by category to identify areas where the user can save.
 #o Compare income and expenses to calculate the user's overall financial health.
-import date
 expenses = [{'rent': 100, 'groceries': 50, 'transportation': 230}]
 income = [{'date':20/2/2024, 'income': 1000}]
 
@@ -17,8 +16,13 @@ def total_expense():
         total+=i['rent']
         total+=i['groceries']
         total+=i['transportation']
-    return total    
+    return total
 
+def total_income():
+    total = 0
+    for i in income:
+        total += i['income']
+    return total
 def total_expense_week():
     total = 0
     iteration = 0
@@ -79,6 +83,51 @@ def add():
     rent = int(input('Enter rent expense : '))
     groceries = int(input('Enter groceries expense : '))
     transportation = int(input('Enter transprtation expense : '))
-    
+    expenses.append({'rent': rent, 'groceries': groceries, 'transportation': transportation})
 
+def incomeadd():
+    date = int(input('Enter Date : '))
+    incomey = int(input('Enter income : '))
+    income.append({'date': date, 'income': incomey})
 
+def stato():
+    if total_income() > total_expense():
+        print(f'Your total expense {total_expense()} is less then your income {total_income()} ')
+        print(f'Thats good u saved {total_income()- total_expense()} rupees')
+    elif total_income() < total_expense():
+        print(f'Your total expense {total_expense()} is greater then your income {total_income()}')
+        print(f'Thas not so good want to knoe more bout it "y" or "not" ')
+        op = input('Enter ur reply : ').lower()
+        if op == 'y':
+            print('.....')
+        elif op == 'n':
+            print('bye :|')
+        else:
+            pass
+def main():
+    while True:
+        print('\t\t\t Personal BUdget tracker ')
+        op = input('Enter A to  add expense or B to add income or R to see the report or anythin else to quite ').lower()
+        if op == 'a':
+            add()
+        elif op == 'b':
+            incomeadd()
+        elif op == 'r':
+            a = input('enter the "w" if you want to see in weekely total or "A" to see all the total \n or you can input "f" to see the financial condition : ').lower()
+            if a == 'a':
+                print(f'grocies total : {spend_on_groceries()}')
+                print(f'transportation totl : {spend_on_transportation()}')
+                print(f'rent total : {spend_on_rent()}')
+            elif a == 'w':
+                print(f'weekely total on grocieries {spend_on_groceries_week()}:')
+                print(f'weekely total on grocieries {spend_on_transportation_week()}:')
+                print(f'weekely total on grocieries {spend_on_rent_week()}:')
+            elif a == 'f':
+                stato()
+            else:
+                pass
+        else:
+            print('Thanks for using our app ')
+            break
+if __name__ == '__main__':
+    main()
